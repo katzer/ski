26755d48bf242811fccaa307bb5c004090f2412e
MrBlaTi <vincent@may-maerkl.de>
2017-01-13 12:39:26 +0100

    refactoring: #10 among other things moved help and usage output methods to main class renamed Misc.go to ErrorHandler.go

--- src/ErrorHandler.go
+++ src/ErrorHandler.go
@@ -6,27 +6,6 @@ import (
 )
 
 /**
-*	Prints the current Version of the goo application
- */
-func printVersion() {
-	os.Stdout.WriteString("0.9")
-}
-
-/**
-*	Prints the help dialog
- */
-func printHelp() {
-	fmt.Println(`usage: goo [options...] <planet>... -c="<command>"`)
-	fmt.Println(`Options:`)
-	fmt.Println(`-s="<path/to/script>", --script="<path/to/script>"  Execute script and return result`)
-	fmt.Println(`-p, --pretty     Pretty print output as a table`)
-	fmt.Println(`-t, --type       Show type of planet`)
-	fmt.Println(`-h, --help       This help text`)
-	fmt.Println(`-v, --version    Show version number`)
-	fmt.Println(`-d, --debug	  Show extended debug informations`)
-}
-
-/**
 *	Formats and prints the given output and error.
  */
 func throwErrOut(out []byte, err error) {
--- src/goo.go
+++ src/goo.go
@@ -64,3 +64,24 @@ func makeExecutor(opts *Opts) Executor {
 	}
 	return executor
 }
+
+/**
+*	Prints the current Version of the goo application
+ */
+func printVersion() {
+	os.Stdout.WriteString("0.9")
+}
+
+/**
+*	Prints the help dialog
+ */
+func printHelp() {
+	fmt.Println(`usage: goo [options...] <planet>... -c="<command>"`)
+	fmt.Println(`Options:`)
+	fmt.Println(`-s="<path/to/script>", --script="<path/to/script>"  Execute script and return result`)
+	fmt.Println(`-p, --pretty     Pretty print output as a table`)
+	fmt.Println(`-t, --type       Show type of planet`)
+	fmt.Println(`-h, --help       This help text`)
+	fmt.Println(`-v, --version    Show version number`)
+	fmt.Println(`-d, --debug	  Show extended debug informations`)
+}
