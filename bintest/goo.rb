a8517b481d06e03032d4d4d9717252fc1e237fc6
MrBlaTi <vincent@may-maerkl.de>
2017-02-14 17:20:08 +0100

    push for hound

--- bintest/goo.rb
+++ bintest/goo.rb
@@ -125,8 +125,7 @@ class TestGoo < Test::Unit::TestCase
                                            'app')
     check_no_error(output, error, 'bad_script')
     assert_false status.success?, 'Process did exit cleanly'
-    assert_include error, 'Process exited with status 127',
-                        'return was not correct'
+    assert_include error, 'Process exited with status 127', 'return incorrect'
   end
 
   def test_bad_command
@@ -134,8 +133,7 @@ class TestGoo < Test::Unit::TestCase
                                            '-d=true', 'app')
     check_no_error(output, error, 'bad_command')
     assert_false status.success?, 'Process did exit cleanly'
-    assert_include error, 'Process exited with status 127',
-                        'return was not correct'
+    assert_include error, 'Process exited with status 127', 'return incorrect'
   end
 
   def test_pretty_print
@@ -143,8 +141,7 @@ class TestGoo < Test::Unit::TestCase
                                            '-d=true', 'app')
     check_error(output, error, 'pretty_print')
     assert_true status.success?, 'Process did not exit cleanly'
-    assert_include output, '0    app                  ',
-                          'return was not correct'
+    assert_include output, '0    app                  ', 'return was incorrect'
   end
 
   def test_multiple_pretty_print
