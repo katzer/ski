51adfb378302cdcbd1132989cda9ea11d5ab27e4
MrBlaTi <vincent@may-maerkl.de>
2017-02-14 16:01:58 +0100

    tuned down the shenanigans a bit

--- bintest/testFolder/scripts/showver.sh
+++ bintest/testFolder/scripts/showver.sh
@@ -1,61 +1,63 @@
 cat <<EOF
-kryptokuerzel version check 7.7
+willywonka version check 2.1
 -----------------------
 
-Willy Wonka chocolate system London
--[drops]---------------------------------------------------------------------
- watergate      5.8.6.9.4
- noodle         5.5.5 (build time Oct 65 2036 39:23:7)
- leipzig        V2.3.4.5 AudiR3 for coffee 1.4
+willywonka blueberrychocolate factory London OompaLoompa_Headquarters
+-[binaries]---------------------------------------------------------------------
+ gateway         4.6.1.1 (build time Oct 21 2015 10:35:11)
+ telhandlerkm    4.6.0.2 (build time Oct 13 2016 12:00:31)
+ leappeng        V4.6.1.4 R19988 for Java 1.7
 
--[overseas projects]-------------------------------------------------------------
- deutschland_dresden        6.6.6.1
- deutschland_berlin         6.6.6.2
- deutschland_senden         6.6.6.3
- deutschland_schottach      6.6.6.4
- deutschland_koeln          6.6.6.5
- deutschland_memmingen      6.6.6.6
- deutschland_kempten        6.6.6.7
- deutschland_muenchen       6.6.6.8
- deutschland_nuernburg      6.6.6.9
- deutschland_bielefeld      6.6.6.0
- deutschland_bielefeld_v2.0 6.6.6.1
- deutschland_saarbruecken   6.6.6.2
- deutschland_miltitz        6.6.6.3
- deutschland_ramstein       6.6.6.4
- deutschland_dillingen      6.6.6.5
- deutschland_ulm            6.6.6.6
+-[gateway projects]-------------------------------------------------------------
+ soap_cp.lpx     4.6.1.5 2016-05-12
+ soap_disp.lpx   4.4.0.0 2011-12-15
+ soap_export.lpx 4.4.0.0 2011-12-15
+ soap_ipsl.lpx   4.4.0.0 2011-12-15
+ soap_ipsq.lpx   4.4.0.0 2011-12-15
+ soap_plc.lpx    4.6.1.2 2015-06-15
+ soap_ps1.lpx    4.6.1.0 2015-11-03
+ soap_ps1_v4.6.1.0.lpx 4.6.1.0 2015-11-03
+ soap_ps2.lpx    4.6.1.0 2015-11-03
+ soap_ps2_v4.6.1.0.lpx 4.6.1.0 2015-11-03
+ soap_ps_v4.6.0.1.lpx 4.6.0.1 2015-03-18
+ soap_sap.lpx    4.6.1.1 2016-06-09
+ soap_sap_old.lpx 4.6.1.0 2015-10-19
+ soap_sap_v4.4.0.5.lpx 4.4.0.5 2013-05-16
+ soap_sap_v4.6.1.0.lpx 4.6.1.0 2015-10-19
+ soap_sap_v4.6.1.1.lpx 4.6.1.1 2016-06-09
 
--[whatisdis]-----------------------------------------------------------------------
- OS              Linux Lorem ipsume some numbers v4.3.76.8 some architecture like x86_64 or arm or something
+-[system]-----------------------------------------------------------------------
+ OS              Linux loremipsum 3.0.101-0.7.40-xen #1 SMP Tue May 10 15:38:20 UTC 2016 (151b869) x86_64 x86_64 x86_64 GNU/Linux
 
 -[unix]-------------------------------------------------------------------------
- USS Linux Enterprise Final Frontier (x86_64)
- VERSION = 100
- PATCHLEVEL = 500
+ SUSE Linux Enterprise Server 11 (x86_64)
+ VERSION = 11
+ PATCHLEVEL = 2
 
--[Lorem]-----------------------------------------------------------------------
-Lorem ipsum fugit tempo gesundheit schneuz
+-[webapp]-----------------------------------------------------------------------
+The import path cannot be accessed. Check the database entry and the rights for the folder application on the WebApp
 
--[surroundings]------------------------------------------------------------------
- myversion          6.6.6.5
- Satans Version     6.6.6.5
- Gods version       6.6.6.5
- Kinkynesslevel     x.x.x.x
+-[database]---------------------------------------------------------------------
+ AdminVersion    4.5.1.2
+ AdminVersions   V4.5.0.0, V4.5.1.1, V4.5.1.2
+ DBVersion       IPS-T 4.6.0.002
+ LC2-Versionen   V1.0.1.0, V1.0.2.1, V1.0.3.0, V1.0.3.1, V1.0.5.1, V1.0.5.2
+ Oracle DB       Oracle Database 12c Enterprise Edition Release 12.1.0.2.0 - 64bit Production
+ SystemVersion   x.x.x.x
 
--[plansforthefuture]----------------------------------------------------------------------
- git-gud        6.23
- amass power    7.38
- ???            2.1
- profit         7.3
+-[scripts]----------------------------------------------------------------------
+ legsysmon.pl    1.20
+ mls-adaptor.pl  2.12
+ mls-config-adaptor.pl 1.2
+ showver.pl      2.1
 
--[fast cars]--------------------------------------------------------------
-l | is.this.real_life | sdf                 | 6.6.6-1               | ARM
-l | is.this.real_life | jwe                 | 6.6.6-6               | ARM
-l | is.this.real_life | hsj                 | 6.6.6-3               | ARM
-l | is.this.real_life | ste                 | 6.6.6-2               | ARM
-l | is.this.real_life | fss                 | 6.6.6-7               | ARM
-l | is.this.real_life | jfs                 | 6.6.6-1               | ARM
-l | is.this.real_life | tqc                 | 6.6.6-8               | ARM
+-[FAST components]--------------------------------------------------------------
+i | pcs_16.1_sles11_ora11204 | adm                 | 2.8.5-3                | i386
+i | pcs_16.1_sles11_ora11204 | km                  | 4.12.5-1               | i386
+i | pcs_16.1_sles11_ora11204 | lfv                 | 1.7.2-1                | i386
+i | pcs_16.1_sles11_ora11204 | mls                 | 4.6.11-1               | i386
+i | pcs_16.1_sles11_ora11204 | nedit               | 5.5.0-1                | i386
+i | pcs_16.1_sles11_ora11204 | qca                 | 4.3.1-1                | i386
+i | pcs_16.1_sles11_ora11204 | qxx                 | 9.4.10-2               | i386
 
 EOF
