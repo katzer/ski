87a6d3dc32d2db9960d9d5244ce198365d183e33
Sebastián Katzer <katzer.sebastian@googlemail.com>
2019-08-13 15:59:02 +0200

    Skip validation for ORBIT_PATH

--- mrblib/ski/job.rb
+++ mrblib/ski/job.rb
@@ -125,7 +125,6 @@ module SKI
     #
     # @return [ Boolean ] true if valid
     def validate_envs
-      raise KeyError,          '$ORBIT_PATH not set'  unless ENV['ORBIT_PATH']
       raise KeyError,          '$ORBIT_KEY not set'   unless ENV['ORBIT_KEY']
       raise File::NoFileError, '$ORBIT_KEY not found' unless File.exist? ENV['ORBIT_KEY']
 
@@ -140,7 +139,8 @@ module SKI
     # @return [ Array<String> ]
     def planets
       query = @spec[:tail].join('" "')
-      cmd   = %(#{ENV['ORBIT_PATH']}/fifa -n -f ski "#{query}")
+      fifa  = ENV.include?('ORBIT_PATH') ? "#{ENV['ORBIT_PATH']}/fifa" : 'fifa'
+      cmd   = %(#{fifa} -n -f ski "#{query}")
       out   = `#{cmd}`
 
       raise "#{cmd} failed with exit code #{$?}" unless $? == 0
